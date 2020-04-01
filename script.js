d44f41d14f56d190aea94866a9531bfe17ffd03a
Ilia Selieznov <ziziron21@gmail.com>
2020-04-02 01:27:17 +0300

    today all data-1

--- script.js
+++ script.js
@@ -4,34 +4,50 @@ let date = new Date();
 // 24 * 3600 * 1000
 // new Date(monthNum.getTime() + 24 * 3600 * 1000).getDate()
 const wrapperDate = document.querySelector('.date__wrapper');
+const containerDate = document.querySelector('.date__container');
 const week = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 let monthNum;
-if( date.getDay() !== 0){
+if (date.getDay() !== 0) {
     monthNum = new Date(date.getTime() - date.getDay() * 24 * 3600 * 1000);
-} else{
-    monthNum =  date.getDate();
+} else {
+    monthNum = date.getDate();
 }
+
 function drawDate() {
     let today = new Date();
-    let k = 2;
-    for (let i = 0; i < week.length; i++){
-        let dayItem = document.createElement('div');
-        dayItem.classList.add('week_day');
-
-        let day = document.createElement('p');
-        day.classList.add('day');
-        day.innerText = week[i];
-
-        let monthWrapper = document.createElement('div');
-        monthWrapper.classList.add('month_wrapper');
-        let month = document.createElement('p');
-        month.classList.add('month');
-        month.innerText = monthNum.getDate();
-        monthNum = new Date((today.getTime() - today.getDay() * 24 * 3600 * 1000) + (k++) * 24 * 3600 * 1000);
-        dayItem.append(day);
-        monthWrapper.append(month);
-        dayItem.append(monthWrapper);
-        wrapperDate.append(dayItem);
+    let k = 1;
+    if (date.getDay() !== 0) {
+        containerDate.style.left = (-date.getDay() + 2) * 115 + 'px';
+    } else {
+        containerDate.style.left = 115 + 'px';
     }
+    for (let i = 0; i < week.length; i++) {
+        function oneDay() {
+            let dayItem = document.createElement('div');
+            dayItem.classList.add('week_day');
+
+            let day = document.createElement('p');
+            day.classList.add('day');
+            day.innerText = week[i];
+
+            let monthWrapper = document.createElement('div');
+            monthWrapper.classList.add('month_wrapper');
+            let month = document.createElement('p');
+            month.classList.add('month');
+            // monthNum = new Date((today.getTime() - today.getDay() * 24 * 3600 * 1000) + (k++) * 24 * 3600 * 1000);
+            month.innerText = new Date((today.getTime() - today.getDay() * 24 * 3600 * 1000) + (k++) * 24 * 3600 * 1000).getDate();
+            dayItem.append(day);
+            monthWrapper.append(month);
+            dayItem.append(monthWrapper);
+            wrapperDate.append(dayItem);
+        }
+
+        oneDay()
+    }
+
+
+
 }
+
 drawDate();
+console.log(date);
