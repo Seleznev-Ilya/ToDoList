1e55a60fc3d14798918366bf2a0898e867810266
Ilia Selieznov <ziziron21@gmail.com>
2020-04-01 22:09:22 +0300

    today data

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-let date = new Date();
-console.log(` ${date.getDate()} ${date.getMonth()}`);
+// let date = new Date();
+// console.log(` ${date.getDate()} ${date.getMonth()}`);
+const wrapperDate = document.querySelector('.date__wrapper');
+const week = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+function drawDate() {
+    let today = new Date();
+    wrapperDate.innerHTML = week[today.getDay() - 1];
+}
+drawDate();
