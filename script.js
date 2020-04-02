018db2dfca92370732458520acdfe966a1ea8f5c
Ilia Selieznov <ziziron21@gmail.com>
2020-04-02 15:29:43 +0300

    date adaptation

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 let date = new Date();
-// console.log(` ${date.getDate()} ${date.getMonth()}`);
-// wrapperDate.innerHTML = week[today.getDay() - 1] + ' ' + today.getDate();
-// 24 * 3600 * 1000
-// new Date(monthNum.getTime() + 24 * 3600 * 1000).getDate()
 const wrapperDate = document.querySelector('.date__wrapper');
 const containerDate = document.querySelector('.date__container');
 const week = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
@@ -16,13 +12,13 @@ if (date.getDay() !== 0) {
 function drawDate() {
     let today = new Date();
     let k = 1;
-    if(document.documentElement.clientWidth <= 414) {
+    if (document.documentElement.clientWidth <= 414) {
         if (date.getDay() !== 0) {
             containerDate.style.left = (-date.getDay() + 2) * (document.documentElement.clientWidth / 3) + 'px';
         } else {
             containerDate.style.left = (document.documentElement.clientWidth / 3) + 'px';
         }
-    } else if(document.documentElement.clientWidth > 414) {
+    } else if (document.documentElement.clientWidth > 414) {
         if (date.getDay() !== 0) {
             containerDate.style.left = (-date.getDay() + 2) * (348 / 3) + 'px';
         } else {
@@ -33,9 +29,9 @@ function drawDate() {
         function oneDay() {
             let dayItem = document.createElement('div');
             dayItem.classList.add('week_day');
-            if(document.documentElement.clientWidth <= 414){
+            if (document.documentElement.clientWidth <= 414) {
                 dayItem.style.width = document.documentElement.clientWidth / 3 + 'px';
-            }else if(document.documentElement.clientWidth > 414) {
+            } else if (document.documentElement.clientWidth > 414) {
                 dayItem.style.width = (348 / 3) + 'px';
             }
 
@@ -47,7 +43,6 @@ function drawDate() {
             monthWrapper.classList.add('month_wrapper');
             let month = document.createElement('p');
             month.classList.add('month');
-            // monthNum = new Date((today.getTime() - today.getDay() * 24 * 3600 * 1000) + (k++) * 24 * 3600 * 1000);
             month.innerText = new Date((today.getTime() - today.getDay() * 24 * 3600 * 1000) + (k++) * 24 * 3600 * 1000).getDate();
             dayItem.append(day);
             monthWrapper.append(month);
@@ -60,4 +55,20 @@ function drawDate() {
 }
 
 drawDate();
-console.log(document.documentElement.clientWidth / 3);
+if (document.documentElement.clientWidth <= 414) {
+    let resize = function (e) {
+        console.log(e);
+        location.reload();
+    };
+    (function () {
+        let time;
+        window.onresize = function (e) {
+            if (time)
+                clearTimeout(time);
+            time = setTimeout(function () {
+                resize(e);
+
+            }, 100);
+        }
+    })();
+}
