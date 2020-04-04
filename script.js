3f90c9f8930a276dff3cb4a1cb893ecab4aaea4a
Ilia Selieznov <ziziron21@gmail.com>
2020-04-05 01:11:34 +0300

    header done 100%

--- script.js
+++ script.js
@@ -1,33 +1,21 @@
-let today = new Date();
-const week = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-const wrapperDate = document.querySelector('.date__wrapper');
-const containerDate = document.querySelector('.date__container');
-let buttonTodayL = document.querySelector('.date_left_today');
-let buttonTodayR = document.querySelector('.date_right_today');
-let resize = function (e) {
-    console.log(e);
-    location.reload();
+const week = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    wrapperDate = document.querySelector('.date__wrapper'), containerDate = document.querySelector('.date__container'),
+    buttonTodayL = document.querySelector('.date_left_today'), buttonTodayR = document.querySelector('.date_right_today'),
+    today = new Date();
+    let time,
+    timer = setTimeout(function step() {
+        if (today.getHours() === 23 && today.getMinutes() === 59) {
+            setTimeout(location.reload(), 10000);
+        }
+        timer = setTimeout(step, 30000);
+    }, 30000);
+window.onresize = function () {
+    if (time)
+        clearTimeout(time);
+    time = setTimeout(function () {
+        location.reload();
+    }, 123);
 };
-(function () {
-    let time;
-    window.onresize = function (e) {
-        if (time)
-            clearTimeout(time);
-        time = setTimeout(function () {
-            resize(e);
-        }, 100);
-    }
-})();
-
-let timer = setTimeout(function step() {
-    upDate();
-    timer = setTimeout(step, 30000);
-}, 30000);
-function upDate(){
-    if (today.getHours() === 23 && today.getMinutes() === 59 ){
-        setTimeout(location.reload(), 10000);
-    }
-}
 
 function drawDates() {
     let k = 1;
@@ -40,18 +28,16 @@ function drawDates() {
         } else if (document.documentElement.clientWidth > 414) {
             dayItem.style.width = (348 / 3) + 'px';
         }
-
         let day = document.createElement('p');
         day.classList.add('day');
         day.innerText = week[i];
-
         let monthWrapper = document.createElement('div');
         monthWrapper.classList.add('month_wrapper');
         let month = document.createElement('p');
         month.classList.add('month');
-        if (today.getDay() !== 0){
+        if (today.getDay() !== 0) {
             month.innerText = new Date((today.getTime() - today.getDay() * 24 * 3600 * 1000) + (k++) * 24 * 3600 * 1000).getDate();
-        } else{
+        } else {
             month.innerText = new Date((today.getTime() - 7 * 24 * 3600 * 1000) + (k++) * 24 * 3600 * 1000).getDate();
         }
         dayItem.append(day);
@@ -60,7 +46,6 @@ function drawDates() {
         wrapperDate.append(dayItem);
     }
 }
-
 
 function highlightDay() {
     let thisDayWrapper = document.querySelector(".date__wrapper");
@@ -73,28 +58,26 @@ function highlightDay() {
     }
 }
 
-
-
 function moveRelevantDate() {
     if (document.documentElement.clientWidth <= 414) {
         if (today.getDay() !== 0) {
             containerDate.style.left = (-today.getDay() + 2) * (document.documentElement.clientWidth / 3) + 'px';
         } else {
-            containerDate.style.left = (-document.documentElement.clientWidth / 3) * 5  + 'px';
+            containerDate.style.left = (-document.documentElement.clientWidth / 3) * 5 + 'px';
         }
     } else if (document.documentElement.clientWidth > 414) {
         if (today.getDay() !== 0) {
             containerDate.style.left = (-today.getDay() + 2) * (348 / 3) + 'px';
-        } else  {
-            containerDate.style.left = (-348 / 3) *5  + 'px';
+        } else {
+            containerDate.style.left = (-348 / 3) * 5 + 'px';
         }
     }
 }
+
 drawDates();
 highlightDay();
 moveRelevantDate();
-
-wrapperDate.addEventListener('click',  (event) =>{
+wrapperDate.addEventListener('click', (event) => {
     let otherDay = event.target;
     for (let i = 0; i < week.length; i++) {
         if (otherDay.closest(`.week_day${i}`)) {
